30954f94d0b929db435883b63baf3ba0b4a32dac
Le Van Sy <syle190104@gmail.com>
2024-07-10 21:41:22 +0700

    sql

---
+++ asm2.sql
@@ -0,0 +1,47 @@
+﻿create database ShopQuanAo_Nhom2_ASM
+
+INSERT INTO products (image, material, name, price, quantity, size, categoryid, description) VALUES
+( 'women1.jpg', N'Được làm từ vải sạn cao cấp, áo có độ bền cao, thoáng mát và mềm mại trên da. Chất liệu vải này không chỉ đẹp mà còn giúp giữ form áo tốt trong suốt quá trình sử dụng', N'ÁO QUÂY SẠN TRẮNG KEM TRẦN CHUN', 400000, 15, 'M', NULL, N'Chiếc áo quây sạn trắng kem trần chun là một món đồ thời trang tinh tế và thanh lịch, hoàn hảo để thêm vào bộ sưu tập thời trang mùa hè của bạn. Với thiết kế đơn giản nhưng không kém phần nổi bật, áo quây này mang lại sự thoải mái và phong cách cho người mặc.<br>-Thiết kế quây trần chun: Thiết kế quây ôm sát vòng ngực kết hợp với chi tiết chun co giãn giúp áo ôm vừa vặn và giữ cố định, đồng thời tạo điểm nhấn đặc biệt cho trang phục. Màu trắng kem thanh lịch dễ dàng kết hợp với nhiều phong cách khác nhau.'),
+( 'women3.jpg', N'Được làm từ vải sạn xanh chất lượng cao, áo có độ mềm mại và thoáng mát, mang lại cảm giác dễ chịu suốt cả ngày dài. Vải sạn giúp giữ form áo tốt và hạn chế nhăn nhúm.', N'ÁO SM SẠN XANH PHỐI GẤU TRẮNG DẬP LY', 450000, 30, 'L', NULL, N'Chiếc áo sơ mi sạn xanh phối gấu trắng dập ly là sự kết hợp hoàn hảo giữa phong cách thanh lịch và sự tươi mới, mang đến cho bạn vẻ ngoài cuốn hút và tinh tế. Áo không chỉ là một món đồ thời trang mà còn là tuyên ngôn về phong cách cá nhân của bạn.<br> - Thiết kế phối gấu trắng:Điểm nhấn của áo nằm ở phần gấu áo màu trắng được phối tinh tế, tạo nên sự tương phản nổi bật và thu hút ánh nhìn. Chi tiết này không chỉ làm tăng tính thẩm mỹ mà còn mang lại sự khác biệt độc đáo cho trang phục.<br> -Phong cách đa dạng: Áo SM sạn xanh phối gấu trắng dập ly có thể dễ dàng kết hợp với nhiều loại trang phục khác nhau như quần âu, chân váy hoặc quần jeans. Bạn có thể diện áo này trong nhiều dịp, từ đi làm, đi học đến đi chơi hay dự tiệc.'),
+( 'women2.jpg', N'Áo được làm từ chất liệu vải CV mềm mại và thoáng mát, mang lại cảm giác thoải mái và dễ chịu suốt cả ngày dài. Vải CV giữ màu tốt và có độ bền cao, giúp áo luôn trông như mới sau nhiều lần giặt.', N'ÁO CV CAM SÁNG XẾP LY BÊN THANH LỊCH', 900000, 20, 'M', NULL, N'Chiếc áo sơ mi sạn xanh phối gấu trắng dập ly là sự kết hợp hoàn hảo giữa phong cách thanh lịch và sự tươi mới, mang đến cho bạn vẻ ngoài cuốn hút và tinh tế. Áo không chỉ là một món đồ thời trang mà còn là tuyên ngôn về phong cách cá nhân của bạn.<br> - Thiết kế phối gấu trắng: Điểm nhấn của áo nằm ở phần gấu áo màu trắng được phối tinh tế, tạo nên sự tương phản nổi bật và thu hút ánh nhìn. Chi tiết này không chỉ làm tăng tính thẩm mỹ mà còn mang lại sự khác biệt độc đáo cho trang phục.<br> -Phong cách đa dạng: Áo SM sạn xanh phối gấu trắng dập ly có thể dễ dàng kết hợp với nhiều loại trang phục khác nhau như quần âu, chân váy hoặc quần jeans. Bạn có thể diện áo này trong nhiều dịp, từ đi làm, đi học đến đi chơi hay dự tiệc.'),
+( 'women4.jpg', N'Vải cao cấp.', N'SET ÁO CHOÀNG DÀI + 2 DÂY ĐEN THANH LỊCH', 500000, 55, 'L', NULL, N'Set Áo Choàng Dài + 2 Dây Đen Thanh Lịch là sự kết hợp hoàn hảo giữa sự thoải mái và phong cách thanh lịch. Được thiết kế để bạn tỏa sáng trong mọi dịp, từ các sự kiện chính trị, buổi tiệc đêm đến ngày làm việc hàng ngày, set này không chỉ mang đến cho bạn vẻ ngoài tự tin mà còn thể hiện phong cách cá nhân độc đáo.<br>-Hướng dẫn bảo quản:<br>Giặt bằng tay hoặc máy ở chế độ nhẹ, nhiệt độ nước dưới 30 độ C.<br>Không sử dụng chất tẩy mạnh, tránh ngâm lâu trong nước.<br>Phơi ngang trên mặt phẳng, tránh phơi trực tiếp dưới ánh nắng mặt trời gắt.<br>Ủi ở nhiệt độ thấp để giữ form dáng áo lâu dài.'),
+( 'men1.jpg', N'Quần được làm từ chất liệu vải cao cấp, mềm mại, thoáng mát và bền đẹp, đảm bảo độ bền và sự thoải mái cho người mặc.', N'QUẦN CARGO NAM TÚI HỘP ỐNG TRƠN FORM WIDE LEG', 700000, 20, 'XL', NULL, N'Quần Cargo Nam Túi Hộp Ống Trơn Form Wide Leg là sự kết hợp hoàn hảo giữa phong cách thời trang và tính ứng dụng. Thiết kế cargo với nhiều túi hộp và ống rộng mang lại sự thoải mái và tiện lợi cho người mặc, đồng thời tạo điểm nhấn cá tính và hiện đại.<br><br><Strong>o Hướng dẫn bảo quản:</Strong><br>-Giặt bằng tay hoặc máy ở chế độ nhẹ, nhiệt độ nước dưới 30 độ C.<br>-Không sử dụng chất tẩy mạnh, tránh ngâm lâu trong nước.<br>-Phơi ngang trên mặt phẳng, tránh phơi trực tiếp dưới ánh nắng mặt trời gắt.<br>-Ủi ở nhiệt độ thấp để giữ form dáng quần lâu dài.'),
+( 'men2.jpg', N'Quần được làm từ chất liệu kaki cao cấp, vừa mềm mại vừa bền bỉ, mang lại cảm giác thoải mái và dễ chịu khi mặc.', N'QUẦN KAKI NAM ỐNG RỘNG PREMIUM FORM WIDE LEG', 1276000, 20, 'XXL', NULL, N'Quần Kaki Nam Ống Rộng Premium Form Wide Leg là lựa chọn hoàn hảo cho những người muốn kết hợp giữa phong cách thời trang và sự thoải mái. Với form quần ống rộng, quần kaki này tạo nên vẻ ngoài ấn tượng và sang trọng, phù hợp cho nhiều dịp khác nhau.<br><br><Strong>o Hướng dẫn bảo quản:</Strong><br>-Giặt bằng tay hoặc máy ở chế độ nhẹ, nhiệt độ nước dưới 30 độ C.<br>-Không sử dụng chất tẩy mạnh, tránh ngâm lâu trong nước.<br>-Phơi ngang trên mặt phẳng, tránh phơi trực tiếp dưới ánh nắng mặt trời gắt.<br>-Ủi ở nhiệt độ thấp để giữ form dáng quần lâu dài.'),
+( 'men3.jpg', N'Vải cao cấp.', N'QUẦN TÂY NAM ỐNG RỘNG PREMIUM FORM LOOSE', 250000, 30, 'XL', NULL, N'Quần Tây Nam Ống Rộng Premium Form Loose là sự kết hợp hoàn hảo giữa phom dáng thời trang và sự thoải mái, phù hợp cho những người muốn tự tin và thoải mái trong mọi hoàn cảnh. Với form quần ống rộng và chất liệu cao cấp, quần tây này mang lại sự tự tin và phong cách cho phái mạnh.<br><br><Strong>o Hướng dẫn bảo quản:</Strong><br>-Giặt bằng tay hoặc máy ở chế độ nhẹ, nhiệt độ nước dưới 30 độ C.<br>-Không sử dụng chất tẩy mạnh, tránh ngâm lâu trong nước.<br>-Phơi ngang trên mặt phẳng, tránh phơi trực tiếp dưới ánh nắng mặt trời gắt.<br>-Ủi ở nhiệt độ thấp để giữ form dáng quần lâu dài.'),
+( 'men5.webp', N'Vải twill.', N'QUẦN ÂU TÂY TWILL ỐNG ÔM XẾP LY TRƠN SLIM CROP', 600000, 20, 'L', NULL, N'Quần Âu Tây Twill Ống Ôm Xếp Ly Trơn Slim Crop là sự lựa chọn hoàn hảo cho những quý ông muốn tạo nên phong cách lịch lãm, hiện đại và cá tính. Với phom dáng ôm slim, chiều dài crop và chất liệu twill cao cấp, sản phẩm này không chỉ mang lại sự thoải mái mà còn tôn lên vẻ đẹp nam tính và phong cách.<br><br><Strong>o Hướng dẫn bảo quản:</Strong><br>-Giặt bằng tay hoặc máy ở chế độ nhẹ, nhiệt độ nước dưới 30 độ C.<br>-Không sử dụng chất tẩy mạnh, tránh ngâm lâu trong nước.<br>-Phơi ngang trên mặt phẳng, tránh phơi trực tiếp dưới ánh nắng mặt trời gắt.<br>-Ủi ở nhiệt độ thấp')
+
+INSERT INTO Categories (id, name) VALUES 
+('C001', N'Áo khoác'),
+('C002', N'Áo sơ mi'),
+('C003', N'Váy'),
+('C004', N'Đầm dự tiệc'),
+('C005', N'Áo thun'),
+('C006', N'Quần dài')
+
+INSERT INTO Account (username, password, fullname, email, phone, adress, role, Createdate) VALUES 
+('sylv', 'sy123', N'Lê Văn Sỹ', 'syle@gmail.com', '0123456789', N'Quận 1', 0, '2023-01-01'),
+('vuingb', 'vuivui', N'Nguyễn Ngọc Bảo Vui', 'vui@gmail.com', '0123789271', N'Quận 5', 0, '2023-07-01'),
+('nhi123', 'hongnhi', N'Nguyễn Thị Hồng Nhi', 'nhi@gmail.com', '0124642457', N'Quận Tân Bình', 0, '2024-01-01'),
+('gianhu', 'nhu999', N'Trần Gia Như', 'nhu@gmail.com', '0198972892', N'Quận 12', 1, '2023-01-01'),
+('thuanvan', 'thuan', N'Phan Văn Thuận', 'thuan@gmail.com', '0123456789', N'Quận 9', 1, '2023-06-04')
+
+INSERT INTO Orders (createdate, status, total_amount, username) VALUES 
+(GETDATE(), N'Chờ xử lý', 3500000, 'sylv'),
+(GETDATE(), N'Đang giao', 2500000, 'gianhu'),
+(GETDATE(), N'Đã giao', 1500000, 'vuingb'),
+(GETDATE(), N'Đã hủy', 500000, 'nhi123'),
+(GETDATE(), N'Chờ xử lý', 4000000, 'thuanvan'),
+(GETDATE(), N'Đang giao', 3000000, 'gianhu'),
+(GETDATE(), N'Đã giao', 4500000, 'sylv'),
+(GETDATE(), N'Đã hủy', 1000000, 'nhi123'),
+(GETDATE(), N'Chờ xử lý', 2000000, 'vuingb')
+
+INSERT INTO Orderdetails (quantity, price, productid, orderid) VALUES 
+(1, 1500000, 1, 1),
+(2, 1250000, 2, 2),
+(1, 1500000, 3, 3),
+(1, 500000, 4, 4),
+(3, 1333333, 5, 5),
+(1, 3000000, 6, 6),
+(2, 2250000, 7, 7),
+(4, 250000, 8, 8)
